94d7f35921707297e6789aa6dd24d02a38bb88dd
George Seeger <georgeseeger@hotmail.com>
2016-12-12 13:09:33 +0000

    removed extraneous lines

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,7 +1,6 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
 
-# ENV['RACK_ENV'] ||= 'development'
 database = ENV['DATABASE_URL'] || "postgres://localhost/makersbnb_#{ENV['RACK_ENV']}"
 DataMapper.setup(:default, database)
 
