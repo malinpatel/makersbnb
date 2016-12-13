fb97b5906d3cd3b2cd5d778ca89d0d529d301270
George Seeger <georgeseeger@hotmail.com>
2016-12-13 10:09:33 +0000

    Improved test and changed error message

--- spec/features/creating_new_requests_spec.rb
+++ spec/features/creating_new_requests_spec.rb
@@ -6,7 +6,7 @@ feature "Creating new requests" do
   scenario "a guest creates a new request" do
     user = User.new(first_name: "Malin", last_name: "Patel", username: "malina", email: "malina@gmail.com", password: "gugu123")
 
-    user_2 = User.new(first_name: "Pea", last_name: "Crystal", username: "pea", email: "pea@gmail.com", password: "secret")
+    user2 = User.new(first_name: "Pea", last_name: "Crystal", username: "pea", email: "pea@gmail.com", password: "secret")
 
     space = Space.new(name: "London Penthouse", description: "3 bed, 1 swimming pool, in-house chef", price: "100", start_date: "2017-01-01", end_date: "2017-12-31")
 
@@ -15,9 +15,8 @@ feature "Creating new requests" do
     sign_up(user2)
 
     visit '/spaces/view'
-    click_button "View property"
-    fill_in "start_date", with: "2017-01-01"
-    fill_in "end_date", with: "2017-01-03"
+    click_link "space-title-#{Space.first.id}"
+    fill_in "date", with: "2017-01-01"
     click_button "Book"
     expect(current_path).to eq('/spaces/view')
     expect(page).to have_content("Your booking request for London Penthouse has been sent to the owner")
