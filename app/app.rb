a9fe8c7dff757483062a9101bd89ee3dfecad2ca
Pea M. Tyczynska <CrystalPea@github.com>
2016-12-13 13:17:18 +0000

    Small fix for app.rb

--- app/app.rb
+++ app/app.rb
@@ -7,3 +7,13 @@ require_relative 'controllers/spaces'
 require_relative 'controllers/sessions'
 require_relative 'controllers/requests'
 require_relative 'server'
+
+class MakersBNB < Sinatra::Base
+  use Rack::MethodOverride
+  enable :sessions
+  set :session_secret, "himitsu"
+  register Sinatra::Flash
+  set :app_file, __FILE__
+
+  run! if app_file == $0
+end
